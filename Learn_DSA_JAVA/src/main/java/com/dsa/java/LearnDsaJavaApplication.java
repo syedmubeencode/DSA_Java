a76a716ffe8692b5784bf0be6493b54d6d924e21
syed <myselfmubeen123@gmail.com>
2024-09-27 09:27:11 +0530

    First commit

---
+++ Learn_DSA_JAVA/src/main/java/com/dsa/java/LearnDsaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.dsa.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDsaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDsaJavaApplication.class, args);
+	}
+
+}
---
+++ Learn_DSA_JAVA/src/test/java/com/dsa/java/LearnDsaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsa.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDsaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
